073f336a3d8ee082d90b3db61f554eda2af68a15
Hércules de Sousa <herculesdeso@gmail.com>
2020-07-02 18:30:08 -0300

    :sparkles: feat: adding image's thumbnail

--- web/src/components/Dropzone/index.tsx
+++ web/src/components/Dropzone/index.tsx
@@ -1,24 +1,35 @@
-import React, {useCallback} from 'react'
+import React, {useCallback, useState} from 'react'
 import {useDropzone} from 'react-dropzone'
 import { FiUpload } from 'react-icons/fi'
 
 import './styles.css'
 
 const Dropzone = () => {
+	const [selectedFileUrl, setSelectedFileUrl] = useState()
+ 
   const onDrop = useCallback(acceptedFiles => {
-    console.log(acceptedFiles)
+		const file = acceptedFiles[0]
+		
+		const fileUrl = URL.createObjectURL(file)
+
+		setSelectedFileUrl(fileUrl)
   }, [])
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({
+		onDrop,
+		accept: 'image/*'
+	})
 
   return (
     <div className='dropzone' {...getRootProps()}>
-      <input {...getInputProps()} />
-      {
-        <p>
-					<FiUpload />
-					Imagem do estabelecimento
-				</p>
-      }
+      <input {...getInputProps()} accept='image/*'/>
+      
+			{ selectedFileUrl 
+				? <img src={selectedFileUrl} alt='Point thumbnail' />
+				: <p>
+						<FiUpload />
+						Imagem do estabelecimento
+					</p>
+			}
     </div>
   )
 }
